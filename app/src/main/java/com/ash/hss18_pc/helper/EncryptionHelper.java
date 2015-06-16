83feb4304c87bc5f9e748b06494f691c1a51f807
HardwinCentral <androkitkat@gmail.com>
2015-06-16 19:17:05 +0530

    new

--- app/src/main/java/com/ash/hss18_pc/helper/EncryptionHelper.java
+++ app/src/main/java/com/ash/hss18_pc/helper/EncryptionHelper.java
@@ -1,13 +1,29 @@
 package com.ash.hss18_pc.helper;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by HSS18-PC on 6/15/2015.
  */
 public class EncryptionHelper {
 
 
-    public static void ConvertToMD5()
+    public static String md5(String s)
     {
-
+        MessageDigest digest;
+        try
+        {
+            digest = MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes(),0,s.length());
+            String hash = new BigInteger(1, digest.digest()).toString(16);
+            return hash;
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
